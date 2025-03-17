7348e7796e4010039365cc028dd6e7cf4294ec48
Karlabrojas <karlabrojasr2@gmail.com>
2025-03-17 13:54:49 -0600

    Ya sube imagenes, no muestra en imageList

--- src/app/page.js
+++ src/app/page.js
@@ -3,200 +3,225 @@
 import React from "react";
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
+import Button from "@mui/material/Button";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
+import { styled } from '@mui/material/styles';
+
 import axios from "axios";
-import { useState , useEffect } from "react";
+import { useState, useEffect } from "react";
 
 export default function Home() {
 
-  const [postImg, setPostImg] = useState(); 
-  const [imgSeleccionada, setImgSeleccionada] = useState(null); 
+  const [postImg, setPostImg] = useState([]);
+  const [imgSeleccionada, setImgSeleccionada] = useState(null);
 
-  useEffect(() =>{
+  useEffect(() => {
     verImage();
-  }, []); 
+  }, []);
 
   const verImage = async () => {
-    const response = await axios.get('http://localhost:3080/images/get')
-    setPostImg(response.data)
+    const result = await axios.get('http://localhost:3080/images/get')
+    console.log("Imágenes obtenidas del backend:", result.data.images);
+    setPostImg(result.data.images)
   }
 
-  const seleccionar = (event) => {
-    setImgSeleccionada(event.target.files[0]);
-};
- 
+  // const seleccionar = (event) => {
+  //   setImgSeleccionada(event.target.value);
+  // };
 
-  const uploadImage = async () => {
-    if(!imgSeleccionada){
-      alert("Selecciona una imagen")
-      return; 
+
+  const uploadImage = async (postImg) => {
+    if (!postImg) {
+      alert("Selecciona una imagen");
+      return;
     }
 
-    const formData = new FormData(); 
-    formData.append("file", setImgSeleccionada); 
+    const formData = new FormData();
+    formData.append("file", postImg[0])
 
-    await axios.post('http//:localhost:3080/images/upload', formData, {
-      headers: { "Content-Type": "multipart/form-data" },
-    })
-    setImgSeleccionada(null); 
-    verImage()
+    const response = await axios.post('http://localhost:3080/images/upload', formData)
   }
+
+  const VisuallyHiddenInput = styled('input')({
+    clip: 'rect(0 0 0 0)',
+    clipPath: 'inset(50%)',
+    height: 1,
+    overflow: 'hidden',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    whiteSpace: 'nowrap',
+    width: 1,
+  });
+
   return (
     <div>
       <div className="mt-5 bg- bg-teal-400 h-20 content-center">
         <h1 className="text-white text-center text-4xl font-semibold font-mono">Galeria de imagagenes -KBRR</h1>
       </div>
-      <div className="bg-white w-full justify-center content-center h-20">
-        {/* <button><img src="../public/image.png"></img></button> */}
-        <input className=" w-96 bg-sky-100" type="file" onChange={seleccionar}/>
-        <button onClick={uploadImage}>Subir imagen</button>
+      <div className=" w-full flex justify-center content-center h-15 mt-10">
+        <Button
+          component="label"
+          role={undefined}
+          variant="contained"
+          tabIndex={-1}
+          startIcon={<CloudUploadIcon />}
+        >
+          Subir imagen
+          <VisuallyHiddenInput
+            type="file"
+            onChange={(event) => uploadImage(event.target.files)}
+            multiple
+          />
+        </Button>
       </div>
       <div className="mt-20">
-      <ImageList sx={{ width: '100%', height: '100%' }} cols={10} rowHeight={250}>
-      {itemData.map((item) => (
-        <ImageListItem key={item.img}>
-          <img
-            srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-            src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
-            alt={item.title}
-            loading="lazy"
-          />
-        </ImageListItem>
-      ))}
-    </ImageList>
+        <ImageList sx={{ width: '100%', height: '100%' }} cols={10} rowHeight={250}>
+          {postImg.map((item) => (
+            <ImageListItem key={item.id}>
+              <img
+                src={item.variants.find((variant) => variant.includes("small"))}
+                alt={item.id}
+                style={{ width: "250px", height: "250px" }}
+                loading="lazy"
+              />
+            </ImageListItem>
+          ))}
+        </ImageList>
       </div>
     </div>
   );
 }
-const itemData = [
-  {
-    img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
-    title: 'Breakfast',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
-    title: 'Burger',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
-    title: 'Camera',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
-    title: 'Coffee',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
-    title: 'Hats',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
-    title: 'Honey',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
-    title: 'Basketball',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
-    title: 'Fern',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
-    title: 'Mushrooms',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
-    title: 'Tomato basil',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
-    title: 'Sea star',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
-    title: 'Bike',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
-    title: 'Tomato basil',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
-    title: 'Sea star',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
-    title: 'Bike',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
-    title: 'Tomato basil',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
-    title: 'Sea star',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
-    title: 'Bike',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
-    title: 'Tomato basil',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
-    title: 'Sea star',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
-    title: 'Bike',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
-    title: 'Tomato basil',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
-    title: 'Sea star',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
-    title: 'Bike',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
-    title: 'Tomato basil',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
-    title: 'Sea star',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
-    title: 'Bike',
-  },{
-    img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
-    title: 'Tomato basil',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
-    title: 'Sea star',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
-    title: 'Bike',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
-    title: 'Tomato basil',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
-    title: 'Sea star',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
-    title: 'Bike',
-  },
-];
+// const itemData = [
+//   {
+//     img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
+//     title: 'Breakfast',
+//   },
+//   {
+//     img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
+//     title: 'Burger',
+//   },
+//   {
+//     img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
+//     title: 'Camera',
+//   },
+//   {
+//     img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
+//     title: 'Coffee',
+//   },
+//   {
+//     img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
+//     title: 'Hats',
+//   },
+//   {
+//     img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
+//     title: 'Honey',
+//   },
+//   {
+//     img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
+//     title: 'Basketball',
+//   },
+//   {
+//     img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
+//     title: 'Fern',
+//   },
+//   {
+//     img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
+//     title: 'Mushrooms',
+//   },
+//   {
+//     img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
+//     title: 'Tomato basil',
+//   },
+//   {
+//     img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
+//     title: 'Sea star',
+//   },
+//   {
+//     img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
+//     title: 'Bike',
+//   },
+//   {
+//     img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
+//     title: 'Tomato basil',
+//   },
+//   {
+//     img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
+//     title: 'Sea star',
+//   },
+//   {
+//     img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
+//     title: 'Bike',
+//   },
+//   {
+//     img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
+//     title: 'Tomato basil',
+//   },
+//   {
+//     img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
+//     title: 'Sea star',
+//   },
+//   {
+//     img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
+//     title: 'Bike',
+//   },
+//   {
+//     img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
+//     title: 'Tomato basil',
+//   },
+//   {
+//     img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
+//     title: 'Sea star',
+//   },
+//   {
+//     img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
+//     title: 'Bike',
+//   },
+//   {
+//     img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
+//     title: 'Tomato basil',
+//   },
+//   {
+//     img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
+//     title: 'Sea star',
+//   },
+//   {
+//     img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
+//     title: 'Bike',
+//   },
+//   {
+//     img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
+//     title: 'Tomato basil',
+//   },
+//   {
+//     img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
+//     title: 'Sea star',
+//   },
+//   {
+//     img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
+//     title: 'Bike',
+//   }, {
+//     img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
+//     title: 'Tomato basil',
+//   },
+//   {
+//     img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
+//     title: 'Sea star',
+//   },
+//   {
+//     img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
+//     title: 'Bike',
+//   },
+//   {
+//     img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
+//     title: 'Tomato basil',
+//   },
+//   {
+//     img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
+//     title: 'Sea star',
+//   },
+//   {
+//     img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
+//     title: 'Bike',
+//   },
+// ];
