f1be72fb22b6d65dbaf432b5674dd99339e3beb1
Karlabrojas <karlabrojasr2@gmail.com>
2025-03-17 19:37:01 -0600

    Ultimas correcciones, funcionalidad en variantes

--- src/app/page.js
+++ src/app/page.js
@@ -17,6 +17,7 @@ import IconButton from "@mui/material/IconButton";
 import CloseIcon from "@mui/icons-material/Close";
 import Toolbar from "@mui/material/Toolbar";
 import Slide from "@mui/material/Slide";
+import Alert from '@mui/material/Alert';
 
 export default function Home() {
   const [postImg, setPostImg] = useState([]);
@@ -24,7 +25,6 @@ export default function Home() {
   const [imgTamaño, setImgTamaño] = useState("medium");
   const [carga, setCarga] = useState(true);
   const [open, setOpen] = useState(false);
-  const [selectedVariants, setSelectedVariants] = useState([]);
 
   useEffect(() => {
     verImage();
@@ -33,7 +33,6 @@ export default function Home() {
   const verImage = async () => {
     try {
       const result = await axios.get("http://localhost:3080/images/get");
-      console.log("Respuesta del backend:", result.data);
       setPostImg(result.data.result.images || []);
     } catch (error) {
       console.error("Error obteniendo imágenes:", error);
@@ -47,10 +46,6 @@ export default function Home() {
   };
 
   const uploadImage = async (files) => {
-    if (!files) {
-      alert("Selecciona una imagen");
-      return;
-    }
 
     const formData = new FormData();
     formData.append("file", files[0]);
@@ -59,25 +54,14 @@ export default function Home() {
     verImage();
   };
 
-  const handleOpen = (variants) => {
-    if (!variants || variants.length === 0) return;
-    setSelectedVariants(variants);
-    const selectedVariant = variants.find((variant) => variant.includes(imgTamaño));
-    setImgSeleccionada(selectedVariant || variants[0]); // Si no encuentra la variante, usa la primera
+  const handleOpen = (imgsrc) => {
+    setImgSeleccionada(imgsrc);
     setOpen(true);
   };
 
-  useEffect(() => {
-    if (open && selectedVariants.length > 0) {
-      const selectedVariant = selectedVariants.find((variant) => variant.includes(imgTamaño));
-      setImgSeleccionada(selectedVariant || selectedVariants[0]);
-    }
-  }, [imgTamaño]);
-
   const handleClose = () => {
     setOpen(false);
-    setImgSeleccionada(""); // Aseguramos que la imagen seleccionada se reinicie
-    setSelectedVariants([]); // También vaciamos la lista de variantes seleccionadas
+    setImgSeleccionada("");
   };
 
   const Transition = React.forwardRef(function Transition(props, ref) {
@@ -96,6 +80,17 @@ export default function Home() {
     width: 1,
   });
 
+  const getImageSize = (size) => {
+    switch (size) {
+      case "public":
+        return { width: "750px", height: "750px" };
+      case "medium":
+        return { width: "500px", height: "500px" };
+      default:
+        return { width: "500px", height: "500px" };
+    }
+  };
+
   return (
     <div>
       <div className="mt-5 bg-teal-400 h-20 content-center">
@@ -112,18 +107,18 @@ export default function Home() {
       </div>
 
       {carga ? (
-        <p className="text-center mt-10">Cargando imágenes...</p>
+        <Alert severity="info">Cargando imágenes</Alert>
       ) : (
-        <div className="mt-20">
+        <div className="mt-20 flex justify-center content-center">
           {postImg.length > 0 ? (
-            <ImageList sx={{ width: "100%", height: 850 }} cols={4} gap={10}>
+            <ImageList sx={{ width: "80%", height: 850 }} cols={4} gap={10}>
               {postImg.map((item) => (
                 <ImageListItem key={item.id}>
                   <img
-                    src={item.variants.find((variant) => variant.includes(imgTamaño))}
+                    src={item.variants.find((variant) => variant.includes("small"))}
                     alt={item.id}
                     style={{ width: "250px", height: "250px", cursor: "pointer" }}
-                    onClick={() => handleOpen(item.variants)}
+                    onClick={() => handleOpen(item.variants.find((variant) => variant.includes(imgTamaño)))}
                   />
                 </ImageListItem>
               ))}
@@ -134,169 +129,30 @@ export default function Home() {
         </div>
       )}
 
-      <Dialog fullScreen open={open} onClose={handleClose} TransitionComponent={Transition}>
-        <AppBar >
+      <Dialog fullScreen open={open} onClose={handleClose}>
+        <AppBar>
           <Toolbar>
             <IconButton edge="start" color="inherit" onClick={handleClose} aria-label="close">
               <CloseIcon />
             </IconButton>
-            <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
-          <InputLabel id="select-label">Tamaño</InputLabel>
-          <Select labelId="select-label" id="select" value={imgTamaño} label="Tamaño" onChange={seleccionar}>
-            <MenuItem value="public">750x750</MenuItem>
-            <MenuItem value="medium">500x500</MenuItem>
-          </Select>
-        </FormControl>
+            <FormControl required sx={{ m: 1, minWidth: 120 }}>
+              <InputLabel id="select-label">Tamaño</InputLabel>
+              <Select labelId="select-label" id="select" value={imgTamaño} label="Tamaño" onChange={seleccionar}>
+                <MenuItem value={"public"}>750x750</MenuItem>
+                <MenuItem value={"medium"}>500x500</MenuItem>
+              </Select>
+            </FormControl>
           </Toolbar>
         </AppBar>
 
-        <DialogContent>
-          {imgSeleccionada ? (
-            <img src={imgSeleccionada} alt="Vista seleccionada" style={{ width: "500px", height: "500px" }} />
-          ) : (
-            <p className="text-center">No hay imagen seleccionada</p>
-          )}
+        <DialogContent sx={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+          <img
+            src={imgSeleccionada || null}
+            alt="Vista seleccionada"
+            style={getImageSize(imgTamaño)} // Aquí aplicamos el tamaño dinámico
+          />
         </DialogContent>
       </Dialog>
     </div>
   );
 }
-
-
-
-
-
-
-// const itemData = [
-//   {
-//     img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
-//     title: 'Breakfast',
-//   },
-//   {
-//     img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
-//     title: 'Burger',
-//   },
-//   {
-//     img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
-//     title: 'Camera',
-//   },
-//   {
-//     img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
-//     title: 'Coffee',
-//   },
-//   {
-//     img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
-//     title: 'Hats',
-//   },
-//   {
-//     img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
-//     title: 'Honey',
-//   },
-//   {
-//     img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
-//     title: 'Basketball',
-//   },
-//   {
-//     img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
-//     title: 'Fern',
-//   },
-//   {
-//     img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
-//     title: 'Mushrooms',
-//   },
-//   {
-//     img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
-//     title: 'Tomato basil',
-//   },
-//   {
-//     img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
-//     title: 'Sea star',
-//   },
-//   {
-//     img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
-//     title: 'Bike',
-//   },
-//   {
-//     img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
-//     title: 'Tomato basil',
-//   },
-//   {
-//     img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
-//     title: 'Sea star',
-//   },
-//   {
-//     img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
-//     title: 'Bike',
-//   },
-//   {
-//     img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
-//     title: 'Tomato basil',
-//   },
-//   {
-//     img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
-//     title: 'Sea star',
-//   },
-//   {
-//     img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
-//     title: 'Bike',
-//   },
-//   {
-//     img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
-//     title: 'Tomato basil',
-//   },
-//   {
-//     img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
-//     title: 'Sea star',
-//   },
-//   {
-//     img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
-//     title: 'Bike',
-//   },
-//   {
-//     img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
-//     title: 'Tomato basil',
-//   },
-//   {
-//     img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
-//     title: 'Sea star',
-//   },
-//   {
-//     img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
-//     title: 'Bike',
-//   },
-//   {
-//     img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
-//     title: 'Tomato basil',
-//   },
-//   {
-//     img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
-//     title: 'Sea star',
-//   },
-//   {
-//     img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
-//     title: 'Bike',
-//   }, {
-//     img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
-//     title: 'Tomato basil',
-//   },
-//   {
-//     img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
-//     title: 'Sea star',
-//   },
-//   {
-//     img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
-//     title: 'Bike',
-//   },
-//   {
-//     img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
-//     title: 'Tomato basil',
-//   },
-//   {
-//     img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
-//     title: 'Sea star',
-//   },
-//   {
-//     img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
-//     title: 'Bike',
-//   },
-// ];
